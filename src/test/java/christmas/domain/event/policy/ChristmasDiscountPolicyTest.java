83fd83b0b4e6d03506f086e236f4dfa1d7660b01
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-15 08:17:18 +0900

    test($ChristmasDiscountPolicy) : ChristmasDiscountPolicy 할인 정책 적용 테스트

--- src/test/java/christmas/domain/event/policy/ChristmasDiscountPolicyTest.java
+++ src/test/java/christmas/domain/event/policy/ChristmasDiscountPolicyTest.java
@@ -1,11 +1,54 @@
 package christmas.domain.event.policy;
 
+import static christmas.domain.menu.Menu.BARBECUE_RIBS;
+import static christmas.domain.menu.Menu.CHOCOLATE_CAKE;
+import static christmas.domain.menu.Menu.T_BONE_STEAK;
+import static christmas.domain.menu.Menu.ZERO_COKE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import christmas.domain.event.EventResultDTO;
+import christmas.domain.event.batch.EventBatch;
+import christmas.domain.menu.Menu;
+import christmas.domain.user.Day;
+import christmas.domain.user.Order;
+import christmas.domain.user.User;
+import java.util.HashMap;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ChristmasDiscountPolicyTest {
+    private ChristmasDiscountPolicy christmasDiscountPolicy;
+
+    public static Stream<Arguments> createUserAndEventResultDto() {
+        User user = createUser();
+        EventResultDTO eventResultDto = EventResultDTO.initEventResultDTO(user.getPriceSum());
+        return Stream.of(
+                Arguments.of(eventResultDto)
+        );
+    }
+
+    private static User createUser() {
+        Day day = new Day(3, "금요일");
+        HashMap<Menu, Integer> orderMenus = new HashMap<>();
+        orderMenus.put(T_BONE_STEAK, 1);
+        orderMenus.put(BARBECUE_RIBS, 1);
+        orderMenus.put(CHOCOLATE_CAKE, 2);
+        orderMenus.put(ZERO_COKE, 1);
+        Order order = new Order(orderMenus);
+        return new User(day, order);
+    }
+
+    @BeforeEach
+    void setUp() {
+        User user = createUser();
+        EventBatch eventBatch = new EventBatch(user);
+        christmasDiscountPolicy = new ChristmasDiscountPolicy(user, eventBatch);
+    }
 
     @Test
     @DisplayName("1,000원으로 시작하여 크리스마스가 다가올수록 날마다 할인 금액이 100원씩 증가합니다.")
@@ -21,5 +64,16 @@ class ChristmasDiscountPolicyTest {
         // THEN
         assertEquals(1_000L, discountPrice1);
         assertEquals(3_400L, discountPrice25);
+    }
+
+    @ParameterizedTest
+    @MethodSource("createUserAndEventResultDto")
+    @DisplayName("크리스마스 할인 조건에 따라 크리스마스 할인을 실행합니다.")
+    void 크리스마스할인_실행(EventResultDTO resultDTO) {
+        // WHEN
+        christmasDiscountPolicy.applyEvent(resultDTO);
+
+        // THEN
+        assertEquals(resultDTO.getAppliedEventPrice().get(christmasDiscountPolicy), 1_200L);
     }
 }
