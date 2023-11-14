082e93b403256222c7c969a0d061e1b351d08d7d
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-15 03:05:42 +0900

    feature($User) : 주문 메뉴 총 가격합 기능 구현

--- src/main/java/christmas/domain/user/User.java
+++ src/main/java/christmas/domain/user/User.java
@@ -1,4 +1,19 @@
 package christmas.domain.user;
 
+import christmas.domain.menu.Menu;
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicReference;
+
 public record User(Day visitDay, Order order) {
+    public Long getPriceSum() {
+        AtomicReference<Long> priceSum = new AtomicReference<>(0L);
+        HashMap<Menu, Integer> orderMenus = order.orderMenus();
+        orderMenus.keySet()
+                .forEach(menu -> {
+                    Integer orderCount = orderMenus.get(menu);
+                    Long price = menu.getPrice();
+                    priceSum.updateAndGet(v -> v + orderCount * price);
+                });
+        return priceSum.get();
+    }
 }
