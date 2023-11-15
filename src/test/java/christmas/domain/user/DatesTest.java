9139b1a3e21f9f60791156e775bb9cfec82189ca
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-15 23:46:47 +0900

    doc($refactor) : 리팩토링 사항들 update

--- src/test/java/christmas/domain/user/DatesTest.java
+++ src/test/java/christmas/domain/user/DatesTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class DatesTest {
     @Test
     @DisplayName("요일에 따른 날짜 시퀀스를 찾습니다.")
-    void 요일에_따른_날짜시퀀스() {
+    void 요일에_따른_날짜시퀀스_해피케이스() {
         // GIVEN
         String dateKor = "금요일";
         // WHEN
